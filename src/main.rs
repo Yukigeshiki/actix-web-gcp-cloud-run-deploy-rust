f9823194b78d90e3df1ecb6a607e1edf8e729232
Laurence <laurence@robothouse.io>
2023-07-12 19:13:12 +0200

    update tracing

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use actix_web_cloud_run::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let subscriber = get_subscriber("actix-web-sqlx-test".into(), "info".into(), std::io::stdout);
+    let subscriber = get_subscriber("actix-web-cloud-run".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
     let listener = TcpListener::bind("0.0.0.0:8080".to_string())?;
